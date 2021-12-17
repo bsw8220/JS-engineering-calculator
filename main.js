508ac9648dddcfa73d5ab237bc05ef04751a8582
bsw8220 <a49208220@gmail.com>
2021-12-17 15:58:28 +0900

    all function updated(still debugging)

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 let expression = "";
+let preAnswer = "";
+
 let showBtns = document.querySelectorAll('button');
 for(let i = 0; i < showBtns.length; i++){
     showBtns[i].addEventListener("click", function(event){
@@ -7,7 +9,13 @@ for(let i = 0; i < showBtns.length; i++){
         }
     });
 }
-// showBtns[0].addEventListener("click", addExp(showBtns[0].value), false);
+
+let operators= new Array();
+let operatorBtns = document.querySelectorAll('.operator');
+for(let i = 0; i < operatorBtns.length; i++){
+    operators.push(operatorBtns[i].value);
+}
+
 function addExp(number){
     if(expression == 0){
         expression = "";
@@ -17,11 +25,67 @@ function addExp(number){
 }
 function getSum(){
     if(expression != ""){
-        document.getElementById("expression").value = expression + "=";
-        expression = expression.replace(/(?<=\D|)\d*\!/, 'factorial('+/(?<=\D|)\d*(?=\!)/.exec(expression)[0]+')').replace(/√/gi,'sqrt')
-        console.log(/(?<=\D|)\d*(?=\!)/g.exec(expression));
-        console.log(expression);
-        document.getElementById("fomula").value = eval(expression);
+        try{
+            //수식 오류 확인 및 제거
+            let lastWords = expression.substring(expression.length-3, expression.length);
+            if(operators.indexOf(lastWords) > -1){
+                expression = expression.substring(0, expression.length-3);
+            }
+
+            //수식 표출
+            document.getElementById("expression").value = expression + " = ";
+
+            //함수 문자열 치환
+            if(/\d*.(?=sin|cos|tan|exp|ln|log|sqrt)/gi.test(expression)){
+                expression = expression.replace(/\d*.(?=sin|cos|tan|exp|ln|log|sqrt)/gi, /\d*.(?=sin|cos|tan|exp|ln|log|sqrt)/gi.exec(expression)[0] +' * ');
+            }
+            if(expression.indexOf("!") > -1  ){
+                expression = expression.replace(/.\!/gi, 'factorial('+/(?<=\D|)\d*(?=\!)/.exec(expression)[0]+')');
+            }
+            if(expression.indexOf("√") > -1){
+                expression = expression.replace(/√/gi,'sqrt');
+            }
+            if(expression.indexOf("×") > -1){
+                expression = expression.replace(/×/gi,'*');
+            }
+            if(expression.indexOf("÷") > -1){
+                expression = expression.replace(/÷/gi,'/');
+            }
+            if(expression.indexOf("^") > -1){
+                expression = expression.replace(/\d*.\^.*\d/gi, `Math.pow(${/\w*(?=\^)/.exec(expression)[0]},${/(?<=\^).*\w/.exec(expression)[0]})`);
+            }
+            if(expression.indexOf("π") > -1){
+                if(/(?<=\D|)\d*\π/.exec(expression)[0] > 0){
+                    expression = expression.replace(/.\π/gi, /(?<=\D|)\d*(?=\π)/.exec(expression)[0] +'*Math.PI');
+                } else {
+                    expression = expression.replace(/π/gi,'Math.PI');
+                }
+            }
+            if(expression.indexOf("e") > -1){
+                console.log(expression);
+                console.log(/(?<=\D|)\d*(?=e\W)/gi.exec(expression));
+                if(/(?<=\D|)\d*e/.exec(expression)[0] > 0){
+                    expression = expression.replace(/.e(?=e\W)/gi, /.(?=e\W)/.exec(expression)[0] +'*Math.E');
+                    
+                } else {
+                    expression = expression.replace(/.e/gi,'Math.E');
+                }
+            }
+            console.log(expression);
+            if(expression.indexOf("Ans") > -1){
+                expression = expression.replace("Ans", preAnswer);
+            }
+            
+
+            //코드 실행
+            document.getElementById("fomula").value = eval(expression);
+            preAnswer = eval(expression);
+            expression = "";
+        } catch(error){
+            console.error(error);
+            alert("수식이 잘못되었습니다.");
+            document.getElementById("fomula").value = "0";
+        }
     } else {
         document.getElementById("fomula").value = "0";
     }
@@ -36,20 +100,30 @@ function cos(number){
     return Math.cos(number*Math.PI/180);
 }
 function exp(number){
-    return Math.exp(number)
+    return Math.exp(number);
 }
 function sqrt(number){
-    return Math.sqrt(number)
+    return Math.sqrt(number);
 }
 function factorial(number) {
-    let result = 1;
-    for (let i = 1; i <= number; i++) {
-      result = result * i;
+    if(number != null){
+        let result = 1;
+        for (let i = 1; i <= number; i++) {
+        result = result * i;
+        }
+        return result;
+    } else {
+        return 0;
     }
-    return result;
+}
+function ln(number){
+    return Math.log(number);
+}
+function log(number){
+    return Math.log(number)/Math.log(10);
 }
 function init(){
     expression = "";
     document.getElementById("fomula").value = "0";
-    document.getElementById("expression").value = "0";
+    document.getElementById("expression").value = `Ans = ${preAnswer}`;
 }
